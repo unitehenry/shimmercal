39aada5262897f38106db348f826843bb99939c1
Henry Unite <henry@lula.is>
2023-08-28 22:02:23 -0400

    get user

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,26 @@
 import Login from './Login';
+import { useEffect, useState } from 'react';
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 function App() {
+
+  const [ user, setUser ] = useState(undefined);
+
+  useEffect(() => {
+    const auth = getAuth();
+    onAuthStateChanged(auth, (user) => setUser(user || null));
+  });
+
+  if (user === undefined) return <div />;
+
+  if (user?.email === 'unitehenry@gmail.com') {
+    return (
+      <div className="flex items-center justify-center h-screen bg-blue-50 p-4">
+        ADMIN
+      </div>
+    );
+  }
+
   return (
     <div className="flex items-center justify-center h-screen bg-blue-50 p-4">
       <Login />
